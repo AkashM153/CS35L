7ce3013d646951f6002d617b9936d8e5fd41161a
Ashish-Basetty <ashish.basetty2@gmail.com>
2023-05-15 19:08:11 -0700

    Added basic rerouting

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Routes, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import SignUpPage from './signup';
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route exact path="/" element={<SignUpPage/>}/>
+                <Route exact path="/signup" element={<SignUpPage/>}/>
+                <Route exact path="/" element={<Navigate to="/signup" />}/>
             </Routes>
         </BrowserRouter>
     );
