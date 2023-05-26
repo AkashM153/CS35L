47a53d32785ab80ff513847045d2a8a8971efae7
Ashish Basetty <ashish.basetty2@gmail.com>
2023-05-25 20:53:10 -0700

    Update eventpage2.js

--- src/pages/eventpage2.js
+++ src/pages/eventpage2.js
@@ -4,14 +4,28 @@ import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
+import Autocomplete from '@mui/material/Autocomplete';
 import TextareaAutosize from '@mui/material/TextareaAutosize';
 import Button from '@mui/material/Button';
 
-export default function EventPage2() {
+const eventTypes = [
+  {label: 'Cultural Performance'},
+  {label: 'Professional Development'},
+  {label: 'Live Music'},
+  {label: 'Social'},
+  {label: 'Info Meeting'},
+  {label: 'Workshop'},
+  {label: 'Greek Life'},
+  {label: 'Networking'},
+  {label: 'Sports'}
+]
+
+export default function EventPage2({ triggerFunc }) {
   const [description, setDescription] = React.useState('');
   const [selectedImage, setSelectedImage] = React.useState(null);
 
   const handleDescriptionChange = (event) => {
+    localStorage.setItem(event.target.id, event.target.value)
     setDescription(event.target.value);
   };
 
@@ -19,35 +33,46 @@ export default function EventPage2() {
     setSelectedImage(event.target.files[0]);
   };
 
+
   return (
     <React.Fragment>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
+      <Grid container spacing={5}>
+        <Grid item xs={12}>
           <TextareaAutosize
             id="description"
             name="description"
             label="Description"
             placeholder="Enter description"
-            minRows={4}
+            minRows={3}
+            defaultValue={localStorage.getItem("description")}
             value={description}
-            onChange={handleDescriptionChange}
+            onInput={handleDescriptionChange}
             style={{ width: '100%' }}
           />
         </Grid>
         
         <Grid item xs={12} md={6}>
-          <TextField
-            required
+          <Autocomplete
+            disablePortal
             id="eventtype"
             name="eventtype"
-            label="Event Type"
-            fullWidth
             autoComplete="family-name"
             variant="standard"
+            defaultValue={localStorage.getItem("eventtype")}
+            options={eventTypes}
+            renderInput={(params) => <TextField {...params} label="Event Type" />}
+            onChange={(event, option) => {
+              if (option== null){
+                localStorage.setItem("eventtype", "")
+              }
+              else{
+                localStorage.setItem("eventtype", option.label)
+              }
+            }}
           />
         </Grid>
 
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12}>
           <input
             accept="image/*"
             id="image-upload"
