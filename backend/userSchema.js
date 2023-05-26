0b1349cd407200b3504dca2eee6a7a5bc645469f
Emily Lu <emilylu162@g.ucla.edu>
2023-05-25 22:30:47 -0700

    Added password hashing to database

--- backend/userSchema.js
+++ backend/userSchema.js
@@ -1,4 +1,6 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'), 
+    bcrypt = require('bcryptjs'),
+    SALT_WORK_FACTOR = 10;
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -22,6 +24,38 @@ const userSchema = new mongoose.Schema({
         type: String
     }
 });
+
+userSchema.pre('save', function(next){
+    const user = this;
+    if(this.isModified("password") || this.isNew){
+        bcrypt.genSalt(SALT_WORK_FACTOR, function(saltError, salt){
+            if(saltError){
+                return next(saltError);
+            }
+            bcrypt.hash(user.password, salt, function(hashError, hash){
+                if(hashError){
+                    return next(hashError);
+                }
+        
+                user.password = hash;
+                next();
+            })
+        })
+    }
+    else{
+        return next();
+    }
+})
+
+/*userSchema.methods.comparePassword = function(password, callback) {
+    bcrypt.compare(password, this.password, function(error, isMatch) {
+      if (error) {
+        return callback(error)
+      } else {
+        return callback(isMatch)
+      }
+    })
+}*/
 
 const User = mongoose.model('User', userSchema);
 
