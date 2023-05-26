0b5bbc728bae28635e617be119240d113dad950d
Ashish Basetty <ashish.basetty2@gmail.com>
2023-05-25 20:52:43 -0700

    Update eventpage1.js

--- src/pages/eventpage1.js
+++ src/pages/eventpage1.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
@@ -16,23 +16,31 @@ import dayjs, { Dayjs } from 'dayjs'; // Import dayjs library and Dayjs type
 export default function EventPage1() {
   const [startTime, setStartTime] = React.useState(null);
   const [endTime, setEndTime] = React.useState(null);
-  const [selectedDate, setSelectedDate] = React.useState(dayjs('2022-04-17'));
-
+  const [selectedDate, setSelectedDate] = React.useState(dayjs());
+  
   const handleStartTimeChange = (value) => {
+    localStorage.setItem("starttime", value.toString())
     setStartTime(value);
   };
 
   const handleEndTimeChange = (value) => {
+    localStorage.setItem("endtime", value.toString())
     setEndTime(value);
   };
   
   const handleDateChange = (date) => {
+    localStorage.setItem("date", date.toString())
     setSelectedDate(date);
   };
+
+  const handleFieldChange = (event) => {
+    localStorage.setItem(event.target.id, event.target.value)
+  }
+
   return (
     <React.Fragment>
-      <Grid container spacing={5}>
-        <Grid item xs={12} sm={6}>
+      <Grid container spacing={5} >
+        <Grid item xs={12} sm={6} >
           <TextField
             required
             id="eventName"
@@ -41,6 +49,9 @@ export default function EventPage1() {
             fullWidth
             autoComplete="given-name"
             variant="standard"
+            defaultValue={localStorage.getItem("eventName")}
+            onChange={handleFieldChange}
+
           />
         </Grid>
 
@@ -53,6 +64,8 @@ export default function EventPage1() {
             fullWidth
             autoComplete="family-name"
             variant="standard"
+            defaultValue={localStorage.getItem("organization")}
+            onChange={handleFieldChange}
           />
         </Grid>
         
@@ -99,72 +112,6 @@ export default function EventPage1() {
             </DemoContainer>
           </LocalizationProvider>
         </Grid>
-
-
-        <Grid item xs={12}>
-        <TextField
-            required
-            id="address1"
-            name="address1"
-            label="Address line 1"
-            fullWidth
-            autoComplete="shipping address-line1"
-            variant="standard"
-        />
-        </Grid>
-        <Grid item xs={12}>
-        <TextField
-            id="address2"
-            name="address2"
-            label="Address line 2"
-            fullWidth
-            autoComplete="shipping address-line2"
-            variant="standard"
-        />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-        <TextField
-            required
-            id="city"
-            name="city"
-            label="City"
-            fullWidth
-            autoComplete="shipping address-level2"
-            variant="standard"
-        />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-        <TextField
-            id="state"
-            name="state"
-            label="State/Province/Region"
-            fullWidth
-            variant="standard"
-        />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-        <TextField
-            required
-            id="zip"
-            name="zip"
-            label="Zip / Postal code"
-            fullWidth
-            autoComplete="shipping postal-code"
-            variant="standard"
-        />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-        <TextField
-            required
-            id="country"
-            name="country"
-            label="Country"
-            fullWidth
-            autoComplete="shipping country"
-            variant="standard"
-        />
-        </Grid>
-        {/* Rest of your form fields */}
       </Grid>
     </React.Fragment>
   );
