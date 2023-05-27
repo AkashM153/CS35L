9be9efe44cce671d6f6dca4635b0e21389239d13
anoushkabhat13 <50841365+anoushkabhat13@users.noreply.github.com>
2023-05-26 20:28:49 -0700

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,3 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import PrimarySearchAppBar from './navbar';
+import MapsComponent from './googlemaps';
+import Listings from './listings';
+import { Grid, Paper, Typography } from '@mui/material';
+
+export default function HomePage() {
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      try {
+        const loc = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        localStorage.setItem('location', JSON.stringify(loc));
+      } catch {
+        localStorage.setItem('location', null);
+      }
+    });
+  }, []);
+
+  const [featuredPosts, setFeaturedPosts] = useState([]);
+
+  // Function to add a new featured post
+  const addFeaturedPost = (newPost) => {
+    setFeaturedPosts((prevPosts) => [...prevPosts, newPost]);
+  };
+
+  return (
+    <>
+      <PrimarySearchAppBar />
+      <Grid container spacing={2} alignItems="center" style={{ minHeight: '100vh', padding: '20px' }}>
+        <Grid item xs={12} md={6}>
+          <MapsComponent />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Grid container spacing={2}>
+            {featuredPosts.map((post) => (
+              <Grid item xs={12} key={post.title}>
+                <Paper elevation={4} sx={{ p: 2 }}>
+                  <Typography variant="h5" component="h3" mb={1}>
+                    {post.title}
+                  </Typography>
+                  <Typography variant="subtitle1" color="text.secondary" mb={1}>
+                    {post.date}  {post.startTime}  {post.endTime}
+                  </Typography>
+                  <Typography variant="body2" mb={2}>
+                    {post.description}
+                  </Typography>
+                </Paper>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Listings setFeaturedPosts={addFeaturedPost} />
+        </Grid>
+      </Grid>
+    </>
+  );
+}
+
+/*
 import PrimarySearchAppBar from "./navbar"
 import MapsComponent from "./googlemaps"
 import React, { useEffect } from 'react'
@@ -34,3 +97,4 @@ export default function HomePage(){
         </>
     )
 }
+*/
